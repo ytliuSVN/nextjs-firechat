38674a0488fc02978225655aa4fab21fd703fee2
penny.liu <penny.liu@kaiostech.com>
2021-04-13 16:49:18 +0800

    Customizing Tailwind installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coolDark: {
+          400: '#40444B',
+          500: '#36393f',
+          600: '#32353b',
+        },
+        primary: {
+          50: '#fbeff3',
+          100: '#d96d91',
+          200: '#d55d85',
+          300: '#d14d79',
+          400: '#cd3c6d',
+          500: '#c33364',
+          600: '#b22e5a',
+          700: '#a22a52',
+          800: '#92264a',
+          900: '#822142',
+        },
+        purple: colors.purple,
+      },
+      boxShadow: {
+        button: 'var(--shadow-button)',
+        card: 'var(--shadow-card)',
+      },
+    },
   },
   variants: {
     extend: {},
