ab847b8697fab5318339f8836e964019a64ec147
penny.liu <penny.liu@kaiostech.com>
2021-04-14 12:03:16 +0800

    Adjust Tailwind CSS config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   <Head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,30 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        coolDark: {
-          400: '#40444B',
-          500: '#36393f',
-          600: '#32353b',
-        },
-        primary: {
-          50: '#fbeff3',
-          100: '#d96d91',
-          200: '#d55d85',
-          300: '#d14d79',
-          400: '#cd3c6d',
-          500: '#c33364',
-          600: '#b22e5a',
-          700: '#a22a52',
-          800: '#92264a',
-          900: '#822142',
-        },
-        purple: colors.purple,
-      },
-      boxShadow: {
-        button: 'var(--shadow-button)',
-        card: 'var(--shadow-card)',
-      },
+      // colors: {
+      //   coolDark: {
+      //     400: '#40444B',
+      //     500: '#36393f',
+      //     600: '#32353b',
+      //   },
+      //   primary: {
+      //     50: '#fbeff3',
+      //     100: '#d96d91',
+      //     200: '#d55d85',
+      //     300: '#d14d79',
+      //     400: '#cd3c6d',
+      //     500: '#c33364',
+      //     600: '#b22e5a',
+      //     700: '#a22a52',
+      //     800: '#92264a',
+      //     900: '#822142',
+      //   },
+      //   purple: colors.purple,
+      // },
+      // boxShadow: {
+      //   button: 'var(--shadow-button)',
+      //   card: 'var(--shadow-card)',
+      // },
     },
   },
   variants: {
