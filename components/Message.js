ba8ac7a4c1bd19f9fb555caf6bac30aab396ab5e
penny.liu <penny.liu@kaiostech.com>
2021-04-14 16:31:23 +0800

    Add appropriate Tailwind classes to Message component

--- components/Message.js
+++ components/Message.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Image from 'next/image';
 import { formatRelative } from 'date-fns';
 
@@ -24,23 +25,41 @@ function Message({
   if (!text) return null;
 
   return (
-    <div>
+    <div className='px-4 py-4 rounded-md hover:bg-gray-50 dark:hover:bg-coolDark-600 overflow-hidden flex items-start'>
       {photoURL ? (
         <Image
           src={photoURL}
           alt='Avatar'
-          className='rounded-full mr-4'
+          className='rounded-full'
           width={45}
           height={45}
         />
       ) : null}
-      {displayName ? <p>{displayName}</p> : null}
-      {createdAt?.seconds ? (
-        <span>{formatDate(new Date(createdAt.seconds * 1000))}</span>
-      ) : null}
-      <p>{text}</p>
+      <div className='ml-4'>
+        <div className='flex items-center mb-1'>
+          {displayName ? (
+            <p className='mr-2 text-primary-500'>{displayName}</p>
+          ) : null}
+          {createdAt?.seconds ? (
+            <span className='text-gray-500 text-xs'>
+              {formatDate(new Date(createdAt.seconds * 1000))}
+            </span>
+          ) : null}
+        </div>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
+
+Message.propTypes = {
+  text: PropTypes.string,
+  // An object taking on a particular shape
+  createdAt: PropTypes.shape({
+    seconds: PropTypes.number,
+  }),
+  displayName: PropTypes.string,
+  photoURL: PropTypes.string,
+};
 
 export default Message;
