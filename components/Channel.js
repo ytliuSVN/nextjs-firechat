19a70e2f3872df9c7aa27ca886e0c359ff51c7df
penny.liu <penny.liu@kaiostech.com>
2021-04-15 11:55:36 +0800

    Add appropriate Tailwind classes to Channel component

--- components/Channel.js
+++ components/Channel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 // Firebase deps
 import firebase from '../shared/configs/firebase';
@@ -6,12 +6,21 @@ import firebase from '../shared/configs/firebase';
 import { Message } from '../components';
 
 function Channel({ user = null }) {
+  const db = firebase.firestore();
+  const messagesRef = db.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(100);
+
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
+
+  const inputRef = useRef(null);
+  const bottomListRef = useRef(null);
+
   const { uid, displayName, photoURL } = user;
 
-  const db = firebase.firestore();
-  const query = db.collection('messages').orderBy('createdAt').limit(100);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   useEffect(() => {
     // Subscribe to query with onSnapshot
@@ -39,7 +48,7 @@ function Channel({ user = null }) {
     const trimmedMessage = newMessage.trim();
     if (db) {
       // Add new message in Firestore
-      db.collection('messages').add({
+      messagesRef.add({
         text: trimmedMessage,
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         uid,
@@ -48,30 +57,57 @@ function Channel({ user = null }) {
       });
       // Clear input field
       setNewMessage('');
+      // Scroll down to the bottom of the list
+      bottomListRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
-    <>
-      <ul>
-        {messages.map((message) => (
-          <li key={message.id}>
-            <Message {...message} />
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={handleOnSubmit}>
-        <input
-          type='text'
-          value={newMessage}
-          onChange={handleOnChange}
-          placeholder='Type your message here...'
-        />
-        <button type='submit' disabled={!newMessage}>
-          Send
-        </button>
-      </form>
-    </>
+    <div className='flex flex-col h-full'>
+      <div className='overflow-auto h-full'>
+        <div className='py-4 max-w-screen-lg mx-auto'>
+          <div className='border-b dark:border-gray-600 border-gray-200 py-8 mb-4'>
+            <div className='font-bold text-3xl text-center'>
+              <p className='mb-1'>Welcome to</p>
+              <p className='mb-3'>NextJS FireChat</p>
+            </div>
+            <p className='text-gray-400 text-center'>
+              This is the beginning of this chat.
+            </p>
+          </div>
+          <ul>
+            {messages.map((message) => (
+              <li key={message.id}>
+                <Message {...message} />
+              </li>
+            ))}
+          </ul>
+          <div ref={bottomListRef} />
+        </div>
+      </div>
+      <div className='mb-6 mx-4'>
+        <form
+          onSubmit={handleOnSubmit}
+          className='flex flex-row bg-gray-200 dark:bg-coolDark-400 rounded-md px-4 py-3 z-10 max-w-screen-lg mx-auto dark:text-white shadow-md'
+        >
+          <input
+            ref={inputRef}
+            type='text'
+            value={newMessage}
+            onChange={handleOnChange}
+            placeholder='Type your message here...'
+            className='flex-1 bg-transparent outline-none'
+          />
+          <button
+            type='submit'
+            disabled={!newMessage}
+            className='uppercase font-semibold text-sm tracking-wider text-gray-500 hover:text-gray-900 dark:hover:text-white transition-colors'
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
