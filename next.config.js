d4125b1945613bb948a87340532c9d7f3088d35b
penny.liu <penny.liu@kaiostech.com>
2021-04-16 15:02:49 +0800

    Styling svg icon with linearGradient

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  webpack: (config, {}) => {
+    // Perform customizations to webpack config
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
